4cac072162b711f0a45ede9caae49babe0c8f53d
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-22 14:05:22 +0800

    Update flashcard.js

--- scripts/flashcard.js
+++ scripts/flashcard.js
@@ -1,1 +1,9 @@
+const flashcard = document.querySelector('.flashcard');
+const flipBtn = document.getElementById('flip-btn');
+let flipped = false;
+
+flipBtn.addEventListener('click', () => {
+  flipped = !flipped;
+  flashcard.classList.toggle('flipped', flipped);
+});
 
